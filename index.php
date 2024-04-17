cfbe9d8e75e737f0e0784749c4e276a8d92089b8
nathan-desilva <159185114+nathan-desilva@users.noreply.github.com>
2024-04-17 22:39:38 +0000

    added favicon

--- index.php
+++ index.php
@@ -3,7 +3,7 @@
 
 <head>
   <meta charset="utf-8" />
-  <meta name="description" content="Volume of a Sphere, in PHP" />
+  <meta name="description" content="Fahrenheit to Celsius, in PHP" />
   <meta name="keywords" content="mths, ics2o" />
   <meta name="author" content="Nathan De Silva" />
   <meta name="viewport" content="width=device-width, initial-scale=1.0" />
@@ -14,7 +14,7 @@
   <link rel="icon" type="image/png" sizes="32x32" href="favicon-32x32.png" />
   <link rel="icon" type="image/png" sizes="16x16" href="favicon-16x16.png" />
   <link rel="manifest" href="site.webmanifest" />
-  <title>Volume of a Sphere, in PHP</title>
+  <title>Fahrenheit to Celsius, in PHP</title>
 </head>
 
 <body>
